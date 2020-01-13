8cad0a68b8950c0f9ec56334b39223030ece34fe
Roman Dubovskyi <rdubowskij@gmail.com>
2020-01-13 14:32:20 +0200

    log4j task done

--- src/main/java/mate/academy/internetshop/controller/InjectInitializer.java
+++ src/main/java/mate/academy/internetshop/controller/InjectInitializer.java
@@ -1,17 +1,20 @@
 package mate.academy.internetshop.controller;
 
 import mate.academy.internetshop.annotations.Injector;
+import org.apache.log4j.Logger;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 public class InjectInitializer implements ServletContextListener {
+    final static Logger LOGGER = Logger.getLogger(InjectInitializer.class);
+
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         try {
             Injector.injectDependency();
         } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
+            LOGGER.error(e);
         }
     }
 
