3522819b7d620a22a504909cacf8e6707deb351c
Roman Dubovskyi <rdubowskij@gmail.com>
2020-01-13 12:27:08 +0200

    changed CompleteOrderController as you suggested in slack

--- src/main/java/mate/academy/internetshop/controller/CompleteOrderController.java
+++ src/main/java/mate/academy/internetshop/controller/CompleteOrderController.java
@@ -20,12 +20,12 @@ public class CompleteOrderController extends HttpServlet {
     private static OrderService orderService;
     @Inject
     private static UserService userService;
+    private static final Long USER_ID = 1L;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String bucketId = req.getParameter("bucket_id");
-        Bucket bucket = bucketService.get(Long.valueOf(bucketId));
-        User user = userService.get(bucket.getOwnerID());
+        Bucket bucket = bucketService.getByOwnerId(USER_ID);
+        User user = userService.get(USER_ID);
         orderService.completeOrder(bucket.getItems(), user);
         req.setAttribute("orders", orderService.getUserOrders(user));
         req.getRequestDispatcher("/WEB-INF/views/allOrders.jsp").forward(req, resp);
